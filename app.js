c0b8479513b05a1f49bf707c870276eb2f82b1d3
Alberto Filippini <filippinialberto@gmail.com>
2018-02-24 11:35:44 +0100

    restart

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+require('./api/data/db.js');
 var express = require('express');
-
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 
-app.set('port',3000);
+var routes = require('./api/routes');
 
-var server = app.listen(app.get('port'),function(){ //funzione di callback
-  var port= server.address().port;
+// Define the port to run on
+app.set('port', 3000);
+
+// Add middleware to console log every request
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next(); 
+});
+
+// Set static directory before defining routes
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable parsing of posted forms
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add some routing
+app.use('/api', routes);
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
   console.log('Magic happens on port ' + port);
-}); //porta su cui xpress ascolta
+});
